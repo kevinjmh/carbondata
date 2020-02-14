10eb45835ae5ad563a25b483504ab4526c017e3a
Manhua <kevinjmh@qq.com>
2020-02-14 10:33:54 +0800

    Update UnsafeIntermediateMerger.java

--- processing/src/main/java/org/apache/carbondata/processing/loading/sort/unsafe/merger/UnsafeIntermediateMerger.java
+++ processing/src/main/java/org/apache/carbondata/processing/loading/sort/unsafe/merger/UnsafeIntermediateMerger.java
@@ -121,7 +121,7 @@ public class UnsafeIntermediateMerger {
     UnsafeIntermediateFileMerger merger =
         new UnsafeIntermediateFileMerger(parameters, intermediateFiles, file);
     if (LOGGER.isDebugEnabled()) {
-      LOGGER.debug("Submitting request for intermediate merging no of files: "
+      LOGGER.debug("Submitting request for intermediate merging number of files: "
               + intermediateFiles.length);
     }
     mergerTask.add(executorService.submit(merger));
